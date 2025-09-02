cc340a64b32ee474856e63dbaa4d3985330ab341
Jaydip Patel <jaydip.patel@spellenx.com>
2025-09-02 12:15:40 +0530

    Update : demo_package.dart -> add multiplication freature

--- lib/demo_package.dart
+++ lib/demo_package.dart
@@ -1,9 +1,11 @@
 library demo_package;
-
 class Calculator {
-  /// Returns [a] + [b]
+ /// Returns [a] + [b]
  static int add(int a, int b) => a + b;
 
-  /// Returns [a] - [b]
+ /// Returns [a] - [b]
  static int subtract(int a, int b) => a - b;
+
+ /// Returns [a] * [b]
+ static int multiply(int a, int b) => a * b;
 }
