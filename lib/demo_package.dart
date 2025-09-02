d6c14008ed72c7a2028e89c504e08141ef499dbe
Jaydip Patel <jaydip.patel@spellenx.com>
2025-09-02 11:29:49 +0530

    Update : demo_package.dart -> make all methods static

--- lib/demo_package.dart
+++ lib/demo_package.dart
@@ -2,8 +2,8 @@ library demo_package;
 
 class Calculator {
   /// Returns [a] + [b]
-  int add(int a, int b) => a + b;
+ static int add(int a, int b) => a + b;
 
   /// Returns [a] - [b]
-  int subtract(int a, int b) => a - b;
+ static int subtract(int a, int b) => a - b;
 }
